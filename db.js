b88a8d2cad52fbd731d6f17533e793179e7a38a1
Omar <omaraboulmaaty.11@gmail.com>
2024-01-12 03:55:18 +0200

    edit 1

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 const db = mysql.createConnection({
-  host: process.env.DATABASE_host,
+  host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
