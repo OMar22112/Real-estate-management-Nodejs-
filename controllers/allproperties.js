3c1bbfc55367e7d8156ef1bb4f6bbe5228f7f8e5
Omar <omaraboulmaaty.11@gmail.com>
2024-01-18 01:13:25 +0200

    show Property

--- controllers/allproperties.js
+++ controllers/allproperties.js
@@ -2,7 +2,6 @@
 import db from '../db.js';
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-
 // Controller function to get all properties with images and user information
 export const getAllProperties = async (req, res) => {
     try {
@@ -24,26 +23,29 @@ export const getAllProperties = async (req, res) => {
                 i.created_at AS image_created_at,
                 COALESCE(a.username, u.username) AS added_by_username,
                 COALESCE(a.type, 'user') AS added_by_type
-                FROM properties p
-                LEFT JOIN images i ON p.id = i.property_id
-                LEFT JOIN users u ON p.user_id = u.id
-                LEFT JOIN admins a ON p.admin_id = a.id
+            FROM properties p
+            LEFT JOIN images i ON p.id = i.property_id
+            LEFT JOIN users u ON p.user_id = u.id
+            LEFT JOIN admins a ON p.admin_id = a.id
         `;
     
         const properties = await db.query(propertiesQuery);
+
+        // Log the properties to the console
+        console.log('Properties:', properties);
     
         // Fetch and append image URLs to the result
         const propertiesWithImageUrls = await Promise.all(properties.map(async property => {
-        const imageUrl = property.image_filename
-            ? await getDownloadURL(ref(getStorage(), `images/${property.image_filename}`), false)
-            : null;
+            const imageUrl = property.image_filename
+                ? await getDownloadURL(ref(getStorage(), `images/${property.image_filename}`), false)
+                : null;
     
             return { ...property, imageUrl };
         }));
     
         res.status(200).json(propertiesWithImageUrls);
-        }catch (error) {
+    } catch (error) {
         console.error('Error retrieving properties:', error);
         res.status(500).json({ error: 'Internal Server Error' });
-        }
+    }
 };
