97a1176be8ebab409fa450047760649ea848a4ca
Omar <omaraboulmaaty.11@gmail.com>
2024-01-18 22:09:18 +0200

    change sql

--- db.js
+++ db.js
@@ -1,36 +1,52 @@
-import mysql from "mysql";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
+import pg from 'pg';
 
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  multipleStatements: true, // Enable multiple statements in one query
-});
+const { Pool } = pg;
 
-// Connect to MySQL
-function connectDatabase() {
-  db.connect((error) => {
-    if (error) {
-      console.error("MySQL connection error:", error.message);
-      setTimeout(connectDatabase, 2000); // Retry connection after 2 seconds
-    } else {
-      console.log("MySQL connected...");
-    }
-  });
-}
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+pool.connect((err)=>{
+  if(err)throw err
+  console.log("Connect to PostgreSql")
+})
 
-// Initial connection
-connectDatabase();
+export default pool;
 
 
-// Handle MySQL connection errors
-db.on("error", (error) => {
-  console.error("MySQL connection error:", error.message);
+// import mysql from "mysql";
+// import dotenv from "dotenv";
+
+// dotenv.config({ path: "./.env" });
+
+// const db = mysql.createConnection({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE,
+//   multipleStatements: true, // Enable multiple statements in one query
+// });
+
+// // Connect to MySQL
+// function connectDatabase() {
+//   db.connect((error) => {
+//     if (error) {
+//       console.error("MySQL connection error:", error.message);
+//       setTimeout(connectDatabase, 2000); // Retry connection after 2 seconds
+//     } else {
+//       console.log("MySQL connected...");
+//     }
+//   });
+// }
+
+// // Initial connection
+// connectDatabase();
+
+
+// // Handle MySQL connection errors
+// db.on("error", (error) => {
+//   console.error("MySQL connection error:", error.message);
   
-});
-
-export default db;
+// });
