63e5af8de7a6be0ac213168f06c2eeecb10fe3fd
Omar <omaraboulmaaty.11@gmail.com>
2024-01-24 22:01:26 +0200

    fix git prperty image 2

--- controllers/allproperties.js
+++ controllers/allproperties.js
@@ -52,20 +52,15 @@ export const getAllProperties = async (req, res) => {
     });
 
     // Create a map of property_id to image_urls
-    const imageMap = imageResults.reduce((acc, result) => {
-      const imageUrl = `${baseUrl}/images/${result.image_filename}`;
-      if (!acc[result.property_id]) {
-        acc[result.property_id] = [imageUrl];
-      } else {
-        acc[result.property_id].push(imageUrl);
-      }
-      return acc;
-    }, {});
+    const imageMap = await Promise.all(imageResults.map(async (result) => {
+      const imageUrl = await getDownloadURL(ref(getStorage(), `images/${result.image_filename}`));
+      return { property_id: result.property_id, imageUrl };
+    }));
 
     // Attach the image URLs array to the property object
     const propertiesWithImages = propertiesResult.map(property => ({
       ...property,
-      imageUrls: imageMap[property.id] || [],
+      imageUrls: imageMap.filter(image => image.property_id === property.id).map(image => image.imageUrl),
     }));
 
     res.status(200).json({
