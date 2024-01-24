ee14656363a65d70e841fc895538a3ba2c29b03f
Omar <omaraboulmaaty.11@gmail.com>
2024-01-25 01:42:51 +0200

    delete image'

--- db.js
+++ db.js
@@ -15,13 +15,8 @@ const db = mysql.createConnection({
 function connectDatabase() {
   db.connect((error) => {
     if (error) {
-      if (error.code === "PROTOCOL_CONNECTION_LOST") {
-        console.error("MySQL connection was closed. Reconnecting...");
-        setTimeout(connectDatabase, 2000); // Retry connection after 2 seconds
-      } else {
-        console.error("MySQL connection error:", error.message);
-        throw error; // You may want to handle this more gracefully in a production environment
-      }
+      console.error("MySQL connection error:", error.message);
+      setTimeout(connectDatabase, 2000); // Retry connection after 2 seconds
     } else {
       console.log("MySQL connected...");
     }
@@ -31,14 +26,11 @@ function connectDatabase() {
 // Initial connection
 connectDatabase();
 
+
 // Handle MySQL connection errors
 db.on("error", (error) => {
-  if (error.code === "PROTOCOL_CONNECTION_LOST") {
-    console.error("MySQL connection was closed. Reconnecting...");
-    connectDatabase();
-  } else {
-    console.error("MySQL connection error:", error.message);
-  }
+  console.error("MySQL connection error:", error.message);
+  
 });
 
 export default db;
