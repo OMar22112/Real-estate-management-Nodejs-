3f0555bb94d3af8702912e92b0ccae6725e8ea5c
Omar <omaraboulmaaty.11@gmail.com>
2024-01-25 02:35:39 +0200

    edit status 4

--- controllers/editpropertystatus.js
+++ controllers/editpropertystatus.js
@@ -7,36 +7,13 @@ initializeApp(firebaseConfig);
 export const editPropertyStatus = async (req, res) => {
   try {
     const propertyId = req.params.propertyId;
-    const { status } = req.body;
+    const { status } = parseInt(req.body);
 
-    // Check if the property exists
-    const existingProperty = await new Promise((resolve, reject) => {
-      db.query('SELECT * FROM properties WHERE id = ?', [propertyId], async (error, results) => {
-        if (error) {
-          console.error('Error checking existing property:', error);
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-
-    if (existingProperty.length === 0) {
-      return res.status(404).json({ message: "Property not found" });
-    }
-
-    console.log('Existing Property:', existingProperty[0]);
-
-    // Update property data in the database
-    const updatedProperty = {
-      status: status || existingProperty[0].status,
-    };
-
-    // Use async/await with the db.query function to update property data
+    // Use async/await with the db.query function to update property status
     await new Promise((resolve, reject) => {
-      db.query("UPDATE properties SET ? WHERE id = ?", [updatedProperty, propertyId], (error, results) => {
+      db.query("UPDATE properties SET status = ? WHERE id = ?", [status, propertyId], (error, results) => {
         if (error) {
-          console.error('Error updating property:', error);
+          console.error('Error updating property status:', error);
           reject(error);
         } else {
           console.log('Property status updated successfully');
