406122c2406aac78287d5dbaddc77b1f243f42be
Omar <omaraboulmaaty.11@gmail.com>
2024-01-25 02:10:30 +0200

    edit status'

--- controllers/editpropertystatus.js
+++ controllers/editpropertystatus.js
@@ -9,11 +9,34 @@ export const editPropertyStatus = async (req, res) => {
     const propertyId = req.params.propertyId;
     const { status } = req.body;
 
-    // Use async/await with the db.query function to update property status
-    await new Promise((resolve, reject) => {
-      db.query("UPDATE properties SET status = ? WHERE id = ?", [status, propertyId], (error, results) => {
+    // Check if the property exists
+    const existingProperty = await new Promise((resolve, reject) => {
+      db.query('SELECT * FROM properties WHERE id = ?', [propertyId], async (error, results) => {
         if (error) {
-          console.error('Error updating property status:', error);
+          console.error('Error checking existing property:', error);
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+
+    if (existingProperty.length === 0) {
+      return res.status(404).json({ message: "Property not found" });
+    }
+
+    console.log('Existing Property:', existingProperty[0]);
+
+    // Update property data in the database
+    const updatedProperty = {
+      status: status || existingProperty[0].status,
+    };
+
+    // Use async/await with the db.query function to update property data
+    await new Promise((resolve, reject) => {
+      db.query("UPDATE properties SET ? WHERE id = ?", [updatedProperty, propertyId], (error, results) => {
+        if (error) {
+          console.error('Error updating property:', error);
           reject(error);
         } else {
           console.log('Property status updated successfully');
