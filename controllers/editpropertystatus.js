8c8dd7f75aea9984cae89f77c83f5c582a8d2e40
Omar <omaraboulmaaty.11@gmail.com>
2024-01-25 02:39:38 +0200

    edit status 5

--- controllers/editpropertystatus.js
+++ controllers/editpropertystatus.js
@@ -7,14 +7,11 @@ initializeApp(firebaseConfig);
 export const editPropertyStatus = async (req, res) => {
   try {
     const propertyId = req.params.propertyId;
-    const { status } = req.body;
-
-    // Parse status as integer (0 or 1)
-    const parsedStatus = parseInt(status) || 0;
+    const status  = parseInt(req.body.status);
 
     // Use async/await with the db.query function to update property status
     await new Promise((resolve, reject) => {
-      db.query("UPDATE properties SET status = ? WHERE id = ?", [parsedStatus, propertyId], (error, results) => {
+      db.query("UPDATE properties SET status = ? WHERE id = ?", [status, propertyId], (error, results) => {
         if (error) {
           console.error('Error updating property status:', error);
           reject(error);
